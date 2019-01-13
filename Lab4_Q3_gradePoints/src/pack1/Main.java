02fd1d669fcc26214c75926900b69e2965926d08
ming0520 <mingblog0520@gmail.com>
2019-01-13 22:15:59 +0800

    Update by Adding Comment
    Adding comment

--- Lab4_Q3_gradePoints/src/pack1/Main.java
+++ Lab4_Q3_gradePoints/src/pack1/Main.java
@@ -5,12 +5,19 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 * Create new object for grade and input
+		 */
 		Grade grade = new Grade();
 		Scanner input = new Scanner(System.in);
 		
+		/*
+		 * Ask user letter grade
+		 */
 		System.out.print("Letter grade: ");
-		grade.setGrade(input.next());
+		grade.setGrade(input.next());//set the grade by its methods
 		
+		//display the proper grade points
 		System.out.println("Grade points: " + grade.getGradePoints());
 		
 		input.close();
