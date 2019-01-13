e395734db4578aec42599824d6c0a1afb7077fa2
ming0520 <mingblog0520@gmail.com>
2019-01-13 19:40:46 +0800

    Update Lab4_Q2_calculateBill DONE

--- Lab4_Q2_calculateBill/src/package1/CalculateBill.java
+++ Lab4_Q2_calculateBill/src/package1/CalculateBill.java
@@ -4,13 +4,35 @@ import java.util.*;
 
 public class CalculateBill {
 	
-	public static final double COUNTRY_TAX_A = 0.07;
-	public static final double COUNTRY_TAX_B = 0.05;
+	public static final double COUNTRY_TAX_A = (double)1.07;
+	public static final double COUNTRY_TAX_Other = (double)1.05;
 	
 
 	public static void main(String[] args) {
-		double purchase,country,taxRate;
+		double purchase,taxRate = 0;
+		char country;
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Amount of purchase? ");
+		purchase = input.nextDouble();
+		
+		System.out.print("Country? ");
+		country = (char)input.next().charAt(0);
+		country = Character.toUpperCase(country);
+		
+		switch(country) {
+			case 'A':
+				taxRate = COUNTRY_TAX_A;
+				break;
+			default:
+				taxRate = COUNTRY_TAX_A;
+				break;
+		}
+
+		System.out.printf("Total Bill: $%,.2f%n", (purchase * taxRate));
+		
+		input.close();
 	}
 
 }
