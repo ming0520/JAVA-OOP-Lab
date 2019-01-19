e846480f835ed3eb2476044c46f7f4dfa0439a10
ming0520 <mingblog0520@gmail.com>
2019-01-19 19:24:47 +0800

    Add Lab4 Q5,Q6,Q7 and Lab5 Q1

--- Lab4_Q5_ficaTax/src/package1/FICATax.java
+++ Lab4_Q5_ficaTax/src/package1/FICATax.java
@@ -1,5 +1,14 @@
 package package1;
 
 public class FICATax {
-
+	public static final float FICA_TAX = (float) 0.0765;
+	float currentEarnings, prevEarnings;
+	
+	public void setCE (float ce) {
+		this.currentEarnings = ce;
+	}
+	
+	public void setPE (float pe) {
+		this.prevEarnings = pe;
+	}
 }
