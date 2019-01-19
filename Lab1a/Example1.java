7ba08e3d96d03e8e9022feb8d188ec2b8ceb2cf3
ming0520 <mingblog0520@gmail.com>
2019-01-19 21:00:31 +0800

    Git igonre added

--- Lab1a/Example1.java
+++
@@ -1,6 +0,0 @@
-public class Example1{
-	public static void main(String args[]){
-		System.out.print("Hello World");
-		System.out.println(". YANA");
-	}
-}
--- README.md
+++
@@ -1,5 +0,0 @@
-# JAVA-OOP-Lab<br>
-<hr>
-Zhong Ming Tan<br>
-QIUP OOP Java Pratical Lab 2018-2019<br>
-November 2018 Academic Session<br>
